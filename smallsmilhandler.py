9a0e7283a952588a59bd9c1dbf43fe2bec4c0d78
Alejandra Delgado Reoyo <areoyo@gamma36.aulas.gsyc.urjc.es>
2016-10-03 15:48:10 +0200

    Guardo datos de etiquetas en diccionarios

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -1,4 +1,70 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+from xml.sax import make_parser
+from xml.sax.handler import ContentHandler
 
+class smallSMILHandler(ContentHandler):
+
+    def __init__ (self):
+        """
+        Constructor. Inicializamos las variables
+        """
+        self.root_layout = {}
+        self.region = {}
+        self.img = {}
+        self.audio = {}
+        self.textstream = {}
+        self.misdatos = []
+       
+    def startElement(self, name, attrs):
+        """
+        Método que se llama cuando se abre una etiqueta
+        """
+        if name == 'root-layout':
+            # De esta manera tomamos los valores de los atributos
+            self.root_layout = {'width': str(attrs.get('width',"")), 
+                                'height': str(attrs.get('height',"")), 
+                                'background-color': str(attrs.get('background-color',""))}
+            print(self.root_layout)                  
+        elif name == 'region':
+            self.region = {'id': str(attrs.get('id',"")),
+                           'top': str(attrs.get('top',"")),
+                           'bottom': str(attrs.get('bottom',"")),
+                           'left': str(attrs.get('left',"")),
+                           'right': str(attrs.get('right',""))}
+            print(self.region)
+        elif name == 'img':     
+            self.img = {'src': str(attrs.get('src',"")),
+                        'region': str(attrs.get('region',"")),
+                        'begin': str(attrs.get('begin',"")),
+                        'dur': str(attrs.get('dur',""))}
+            print(self.img)
+        elif name == 'audio':
+            self.audio = {'src': str(attrs.get('src',"")),
+                          'begin': str(attrs.get('begin',"")),
+                          'dur': str(attrs.get('dur', ""))}
+            print(self.audio)
+        elif name == 'textstream':
+            self.textstream = {'src': str(attrs.get('src',"")),
+                               'region': str(attrs.get('region',""))}
+            print(self.textstream)
+    def get_tags (self):
+        """
+        Método que se llama para guardar la lista de datos
+        """
+
+        
+        return self.misdatos
+        
+if __name__ == "__main__":
+    """
+    Programa principal
+    """
+    parser = make_parser()
+    kHandler = smallSMILHandler()
+    parser.setContentHandler(kHandler)
+    parser.parse(open('karaoke.smil'))
+    mis_datos = kHandler.get_tags()
+
+        
