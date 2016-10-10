37fbb4cd3712f7825f91f09a234ee2fd6dd33e99
Alejandra Delgado Reoyo <areoyo@gamma28.aulas.gsyc.urjc.es>
2016-10-10 11:57:08 +0200

    PEP-8

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -4,31 +4,32 @@
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
+
 class smallSMILHandler(ContentHandler):
 
-    def __init__ (self):
+    def __init__(self):
         """
         Constructor. Inicializamos las variables
         """
         self.misdatos = []
-        self.atributos = {'root-layout': ['width', 'height', 'background-color'],
+        self.attrs = {'root-layout': ['width', 'height', 'background-color'],
                           'region': ['id', 'top', 'bottom', 'left', 'right'],
-                          'img': ['src', 'region','begin', 'dur'],
+                          'img': ['src', 'region', 'begin', 'dur'],
                           'audio': ['src', 'begin', 'dur'],
                           'textstream': ['src', 'region']}
 
-    def startElement(self, name, attrs):
+    def startElement(self, name, atributes):
         """
         Método que se llama cuando se abre una etiqueta
         """
-        dicc = {}        
-        if name in self.atributos:
+        dicc = {}
+        if name in self.attrs:
             dicc = {'tag': name}
-            for atributo in self.atributos[name]:
-                dicc[atributo] = str(attrs.get(atributo,"")) 
+            for atributo in self.attrs[name]:
+                dicc[atributo] = str(atributes.get(atributo, "")) 
             self.misdatos.append(dicc)
 
-    def get_tags (self):
+    def get_tags(self):
         """
         Método que se llama para guardar la lista de datos
         """
